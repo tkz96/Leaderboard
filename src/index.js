a7db18ee9192426c0ec1aa2c0fa53e14d3fe2f88
Talha <tkz9696@gmail.com>
2022-05-19 02:06:25 +0500

    Add event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+
+const form = document.querySelector('form');
+const refreshScores = document.getElementById('refresh-scores');
+
+window.addEventListener('DOMContentLoaded', () => {
+    form.addEventListener('submit', (e) => sendScoreToAPI(e));
+    refreshScores.addEventListener('click', refreshScores());
+});
