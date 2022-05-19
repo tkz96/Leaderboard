641e7f4ab77340f414f776fe6f063a223297abe2
talha <tkz9696@gmail.com>
2022-05-19 17:37:58 +0500

    Basic API code

--- src/games.js
+++ src/games.js
@@ -1,1 +1,25 @@
 const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const sendScoreToAPI = async (name, score) => {
+    await fetch(
+        `'${BASE_URL}/games/lQk6PtMS8WHtKZi99VDH/scores'`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                user: name,
+                score,
+            }),
+        }
+    ).then((response) => response.json());
+};
+
+const refreshScores = async () => {
+    await fetch(
+        'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LzQ66iqiKtZlXDNStqI0/scores',
+    )
+    .then((response) => response.json())
+    .then((json) => renderUi(json));
+};
